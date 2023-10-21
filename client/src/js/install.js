cf58726378777869f8ed696529030bfe41d163ef
mhorton <horton-michael@users.noreply.github.com>
2023-10-20 21:06:58 -0400

    adds click event on Install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,10 +5,15 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   butInstall.style.visibility = 'visible';
-});
+  butInstall.textContent = 'Click the button to install!';
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  // TODO: Implement a click event handler on the `butInstall` element
+  butInstall.addEventListener('click', async () => {
+    event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+  });
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
